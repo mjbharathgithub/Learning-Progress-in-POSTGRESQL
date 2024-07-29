e8651f6440a0647c3106e2c97e02c9a5d2cfb8a2
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2024-07-29 10:25:06 +0530

    Day 2 of sql

--- SQL/Queries/day1.sql
+++ SQL/Queries/day1.sql
@@ -1,5 +1,5 @@
-drop table if exists student;
-create table student(
+1.drop table if exists student;
+2.create table student(
  student_id serial,
 	first_name text ,
 	last_name text ,
@@ -9,14 +9,14 @@ create table student(
 );
 
 drop table if exists program_table;
-create table program_table(
+3.create table program_table(
 student_ref_id int,
 program_name text,
 program_start_date timestamp,
 foreign key(student_ref_id) references student(student_id)
 );
 
-drop table if exists scholarShip;
+4.drop table if exists scholarShip;
 create table scholarship(
 student_ref_id int,
 scholarship_amount bigint,
@@ -24,21 +24,49 @@ scholarship_date timestamp,
 foreign key(student_ref_id) references student(student_id)
 );
 
---change the first_name to uppercase and print it--
+5)--change the first_name to uppercase and print it--
 select upper(first_name) from student;
 
---select firs_name whose first character is in capital--
+6)--select firs_name whose first character is in capital--
 select first_name from student
 where substring(first_name from 1 for 1)=upper(substring(first_name from 1 for 1));
 
---select distinct subjects --
+7)--select distinct subjects --
 select distinct(major) from student order by major asc; -- or select major from student group by student;--
 
---select first three character in a string--
+8)--select first three character in a string--
 select substring(first_name from 1 for 3) from student ;
 
---select distinct string and print their length--
+9)--select distinct string and print their length--
 select major,length(major) from student group by(major) order by length(major) asc; --or  SELECT DISTINCT MAJOR, LENGTH(MAJOR) FROM Student;--
 
 -- replace 'a' with 'A' in first_name and print it--
-select replace(first_name,'a','A') from student;
+10)select replace(first_name,'a','A') from student;
+
+
+--------------------------------------------------------------------- DAY 2 -------------------------------------------------------------------------------------
+
+11)--To concat two strings from two columns and print it as a single column--
+select concat(first_name,' ',last_name) as "Complete Name" from student;
+-- OR--
+select first_name||' '||last_name as "Complete Name" from student;
+
+12)--select first_name in asc and major in desc --	
+SELECT first_name, major, gpa
+FROM Student
+ORDER BY first_name ASC, major DESC;
+
+13)--select specific student details from the table USED IN which acts as multiple OR--
+select *
+from student
+where first_name in('Prem','Shivansh');
+
+14)--SQL query to print details of the Students excluding FIRST_NAME as ‘Prem’ and ‘Shivansh’ from Student table.--
+select *
+from student
+where first_name not in('Prem','Shivansh');
+
+15)--Write a SQL query to print details of the Students whose FIRST_NAME ends with ‘a’.--
+select *
+from student
+where first_name like '%a';
